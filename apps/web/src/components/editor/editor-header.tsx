78a0a27a72e5e6c3d349abe2cb28ce6cd1e64d83
Maze Winther <mazewinther@gmail.com>
2026-04-14 21:45:11 +0200

    feat: editor feedback

--- apps/web/src/components/editor/editor-header.tsx
+++ apps/web/src/components/editor/editor-header.tsx
@@ -15,6 +15,7 @@ import { DeleteProjectDialog } from "./dialogs/delete-project-dialog";
 import { useRouter } from "next/navigation";
 import { FaDiscord } from "react-icons/fa6";
 import { ExportButton } from "./export-button";
+import { FeedbackPopover } from "@/lib/feedback/components/feedback-popover";
 import { ThemeToggle } from "../theme-toggle";
 import { DEFAULT_LOGO_URL } from "@/lib/site/brand";
 import { SOCIAL_LINKS } from "@/lib/site/social";
@@ -34,6 +35,7 @@ export function EditorHeader() {
 				<EditableProjectName />
 			</div>
 			<nav className="flex items-center gap-2">
+				<FeedbackPopover />
 				<ExportButton />
 				<ThemeToggle />
 			</nav>
